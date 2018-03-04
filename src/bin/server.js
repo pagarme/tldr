e066fb8538d5dbb1d12754d52b2ce0143a59c7cb
Wilk Maia <wilkmaia@gmail.com>
2018-03-04 20:52:01 -0300

    server: add api get request to serve receipt data

--- src/bin/server.js
+++ src/bin/server.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const database = require('../database')
+const receiptController = require('../controllers/receipt')
 
 const app = express()
+
+app.get(
+  '/api/receipt/:receipt_id',
+  receiptController.show
+)
 
 database.bootstrap()
   .then(() => app.listen(process.env.PORT))
