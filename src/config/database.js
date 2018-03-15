d6a70d41efa1f30dd554863f25a9e6d4972a8051
Wilk Maia <wilkmaia@gmail.com>
2018-03-15 16:07:38 -0300

    env: add sandbox and live variables
    Instead of dealing with only one kind of variable, we add to the `.env`
    file the application environment specific variables.

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,40 @@ const {
   getConfig,
 } = require('./')
 
+let host
+let database
+let username
+let password
+let port
+
+if (process.env.APP_ENV === 'live') {
+  host = process.env.LIVE_DATABASE_URL
+  database = process.env.LIVE_DATABASE_NAME
+  username = process.env.LIVE_DATABASE_USERNAME
+  password = process.env.LIVE_DATABASE_PASSWORD
+  port = process.env.LIVE_DATABASE_PORT
+} else if (process.env.APP_ENV === 'sandbox') {
+  host = process.env.SANDBOX_DATABASE_URL
+  database = process.env.SANDBOX_DATABASE_NAME
+  username = process.env.SANDBOX_DATABASE_USERNAME
+  password = process.env.SANDBOX_DATABASE_PASSWORD
+  port = process.env.SANDBOX_DATABASE_PORT
+} else {
+  host = process.env.DATABASE_URL
+  database = process.env.DATABASE_NAME
+  username = process.env.DATABASE_USERNAME
+  password = process.env.DATABASE_PASSWORD
+  port = process.env.DATABASE_PORT
+}
+
 const config = {
   development: {
-    host: process.env.DATABASE_URL,
+    host,
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    database,
+    username,
+    password,
+    port,
     logging: false,
   },
   test: {
@@ -22,12 +48,12 @@ const config = {
     logging: false,
   },
   production: {
-    host: process.env.DATABASE_URL,
+    host,
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    database,
+    username,
+    password,
+    port,
     logging: false,
   },
 }
--- src/config/queues.js
+++ src/config/queues.js
@@ -2,22 +2,32 @@ const {
   getConfig,
 } = require('./')
 
+let endpoint
+
+if (process.env.APP_ENV === 'live') {
+  endpoint = process.env.LIVE_RECEIPTS_QUEUE_URL
+} else if (process.env.APP_ENV === 'sandbox') {
+  endpoint = process.env.SANDBOX_RECEIPTS_QUEUE_URL
+} else {
+  endpoint = process.env.RECEIPTS_QUEUE_URL
+}
+
 const config = {
   development: {
     receipts: {
-      endpoint: process.env.RECEIPTS_QUEUE_URL,
+      endpoint,
       concurrency: 10,
     },
   },
   test: {
     receipts: {
-      endpoint: process.env.RECEIPTS_QUEUE_URL,
+      endpoint,
       concurrency: 10,
     },
   },
   production: {
     receipts: {
-      endpoint: process.env.RECEIPTS_QUEUE_URL,
+      endpoint,
       concurrency: 10,
     },
   },
