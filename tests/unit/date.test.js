f2f56fdbfdfa1cc802bd101f9aa9abec7350fdb3
Matheus Macabu <macabu.matheus@gmail.com>
2018-04-09 18:34:06 -0300

    date: fix to use correct timezone

--- tests/unit/date.test.js
+++ tests/unit/date.test.js
@@ -2,10 +2,10 @@ const formatDate = require('../../src/lib/date')
 
 describe('Date Lib', () => {
   test('formatDate should return \'DD/MM/YYYY - HH:mm:SS\' for a valid input', () => {
-    expect(formatDate('2018-03-02T10:12:25.000Z')).toBe('02/03/2018 - 10:12:25')
+    expect(formatDate('2018-03-02T10:12:25.000Z')).toBe('02/03/2018 - 07:12')
   })
 
-  test('formatDate should return the input when given an invalid date', () => {
-    expect(formatDate(124)).toBe(124)
+  test('formatDate should return UNIX epoch `-10800` when given an invalid date', () => {
+    expect(formatDate(0)).toBe('31/12/1969 - 21:00')
   })
 })
