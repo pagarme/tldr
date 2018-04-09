ec1edb7926f13a25b4445199cbc7842cd66e1958
Matheus Macabu <macabu.matheus@gmail.com>
2018-04-09 10:46:34 -0300

    receipt: send correct phone data to receipt
    we should respect international phone templates

--- src/lib/phone.js
+++ src/lib/phone.js
@@ -1,9 +1,23 @@
-const formatPhone = (n) => {
-  if (n.length < 10 || n.length > 11) {
-    return n
+const formatPhone = (phoneNumber) => {
+  let phone = phoneNumber
+
+  if (phoneNumber[0] === '+') {
+    phone = phoneNumber.slice(1, phone.length)
   }
-  const m = n.length - 10
-  return `(${n.slice(0, 2)}) ${n.slice(2, 6 + m)}-${n.slice(6 + m, n.length)}`
+
+  if (
+    phone.length < 12 ||
+    phone.length > 13 ||
+    !phone.startsWith('55')
+  ) {
+    return phone
+  }
+
+  const trailingDigit = phone.length - 12
+
+  return `+${phone.slice(0, 2)} (${phone.slice(2, 4)}) \
+${phone.slice(4, 8 + trailingDigit)}\
+-${phone.slice(8 + trailingDigit, phone.length)}`
 }
 
 module.exports = formatPhone
--- tests/unit/phone.test.js
+++ tests/unit/phone.test.js
@@ -1,12 +1,12 @@
 const formatPhone = require('../../src/lib/phone')
 
 describe('Phone Lib', () => {
-  test('formatPhone should return \'(21) 98765-4321\' when given \'21987654321\'', () => {
-    expect(formatPhone('21987654321')).toBe('(21) 98765-4321')
+  test('formatPhone should return \'+55 (21) 98765-4321\' when given \'+5521987654321\'', () => {
+    expect(formatPhone('+5521987654321')).toBe('+55 (21) 98765-4321')
   })
 
-  test('formatPhone should return \'(21) 3021-4567\' when given \'2130214567\'', () => {
-    expect(formatPhone('2130214567')).toBe('(21) 3021-4567')
+  test('formatPhone should return \'(21) 3021-4567\' when given \'552130214567\'', () => {
+    expect(formatPhone('552130214567')).toBe('+55 (21) 3021-4567')
   })
 
   test('formatPhone should return the input when given an invalid phone', () => {
