828b9d6048c3617660961ec5dd298f02846d6714
Allan Jorge <allan.jorge@pagar.me>
2018-07-24 14:41:14 -0300

    tests: add receipt template tests

---
+++ tests/unit/helper.js
@@ -0,0 +1,24 @@
+const receiptData = {
+  transaction_id: 12345,
+  receipt_id: 'abc-123',
+  seller_id: 'loja123',
+  seller_name: 'Loja 1 2 3',
+  transaction_status: 'refunded',
+  amount: 987,
+  payment_date: '2018-03-02 10:12:25Z',
+  event_date: '2018-03-22 15:12:25Z',
+  card_holder_name: 'Senny Bings',
+  card_number_last_digits: '1234',
+  card_brand: 'VISA',
+  installments: 2,
+  phone_number: '+552227987654',
+  cvm_pin: true,
+  payment_method: 'credit_card',
+  capture_method: 'emv',
+  authorization_code: '4DDP1X',
+  aid: '02199520',
+  application_cryptogram: '5EC8B98ABC8F9E7597647CBCB9A75400',
+  soft_descriptor: 'pg* Loja 1 2 3',
+}
+
+module.exports = { receiptData }
---
+++ tests/unit/receipt.test.js
@@ -0,0 +1,257 @@
+const ejs = require('ejs')
+const formatMoney = require('../../src/lib/money')
+const formatPhone = require('../../src/lib/phone')
+const formatDate = require('../../src/lib/date')
+const formatPaymentMethod = require('../../src/lib/payment-method')
+const formatCaptureMethod = require('../../src/lib/capture-method')
+const formatCardBrand = require('../../src/lib/card-brand')
+const { receiptData } = require('./helper')
+
+const TEMPLATE_PATH = './views/pages/receipt.ejs'
+
+describe('Rendered receipt template', () => {
+  describe('with a credit card', () => {
+    let renderedTemplate
+
+    beforeAll(async () => {
+      const receipt = Object.assign({}, receiptData)
+
+      const data = {
+        receiptAmount: formatMoney(receipt.amount),
+        receiptPhone: formatPhone(receipt.phone_number),
+        receiptPaymentMethod: formatPaymentMethod(receipt.payment_method),
+        receiptCaptureMethod: formatCaptureMethod(receipt.capture_method),
+        receiptCardBrand: formatCardBrand(receipt.card_brand),
+        receiptDate: formatDate(receipt.payment_date),
+        receipt,
+      }
+
+      return ejs.renderFile(
+        TEMPLATE_PATH,
+        data,
+        {
+          rmWhitespace: true,
+        },
+        (err, str) => {
+          renderedTemplate = str
+        }
+      )
+    })
+
+    test('should have seller name \'Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Loja 1 2 3'))
+    })
+
+    test('should have payment date \'2018-03-02 10:12:25\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('02/03/2018 - 07:12'))
+    })
+
+    test('should have line \'na fatura do seu cartão como\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('na fatura do seu cartão como'))
+    })
+
+    test('should have soft descriptor \'pg* Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('pg* Loja 1 2 3'))
+    })
+
+    test('should have amount \'9,87\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('9,87'))
+    })
+
+    test('should have installments \'2\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('2'))
+    })
+
+    test('should have \'Bandeira\' \'Visa Crédito\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Visa Crédito'))
+    })
+
+    test('should have \'Últimos dígitos\' \'4242\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('1234'))
+    })
+
+    test('should have \'ID da transação\' \'12345\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('12345'))
+    })
+
+    test('should have \'AUT\' \'4DDP1X\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('4DDP1X'))
+    })
+
+    test('should have \'Código do lojista\' \'loja123\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('loja123'))
+    })
+
+    test('should have \'AC\' \'ONL-CHIP\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('ONL-CHIP'))
+    })
+
+    test('should have line \'Transação autorizada mediante uso de senha pessoal', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Transação autorizada mediante uso de senha pessoal'))
+    })
+  })
+
+  describe('with a debit card', () => {
+    let renderedTemplate
+
+    beforeAll(async () => {
+      const receipt = Object.assign({}, receiptData)
+      receipt.payment_method = 'debit_card'
+
+      const data = {
+        receiptAmount: formatMoney(receipt.amount),
+        receiptPhone: formatPhone(receipt.phone_number),
+        receiptPaymentMethod: formatPaymentMethod(receipt.payment_method),
+        receiptCaptureMethod: formatCaptureMethod(receipt.capture_method),
+        receiptCardBrand: formatCardBrand(receipt.card_brand),
+        receiptDate: formatDate(receipt.payment_date),
+        receipt,
+      }
+
+      return ejs.renderFile(
+        TEMPLATE_PATH,
+        data,
+        {
+          rmWhitespace: true,
+        },
+        (err, str) => {
+          renderedTemplate = str
+        }
+      )
+    })
+
+    test('should have seller name \'Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Loja 1 2 3'))
+    })
+
+    test('should have payment date \'2018-03-02 10:12:25\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('02/03/2018 - 07:12'))
+    })
+
+    test('should have line \'no seu extrato como\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('no seu extrato como'))
+    })
+
+    test('should have soft descriptor \'pg* Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('pg* Loja 1 2 3'))
+    })
+
+    test('should have amount \'9,87\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('9,87'))
+    })
+
+    test('should have installments \'2\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('2'))
+    })
+
+    test('should have \'Bandeira\' \'Visa Crédito\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Visa Débito'))
+    })
+
+    test('should have \'Últimos dígitos\' \'4242\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('1234'))
+    })
+
+    test('should have \'ID da transação\' \'12345\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('12345'))
+    })
+
+    test('should have \'AUT\' \'4DDP1X\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('4DDP1X'))
+    })
+
+    test('should have \'Código do lojista\' \'loja123\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('loja123'))
+    })
+
+    test('should have \'AC\' \'ONL-CHIP\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('ONL-CHIP'))
+    })
+
+    test('should have line \'Transação autorizada mediante uso de senha pessoal', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Transação autorizada mediante uso de senha pessoal'))
+    })
+  })
+
+  describe('without cvm pin', () => {
+    let renderedTemplate
+
+    beforeAll(async () => {
+      const receipt = Object.assign({}, receiptData)
+      receipt.cvm_pin = false
+
+      const data = {
+        receiptAmount: formatMoney(receipt.amount),
+        receiptPhone: formatPhone(receipt.phone_number),
+        receiptPaymentMethod: formatPaymentMethod(receipt.payment_method),
+        receiptCaptureMethod: formatCaptureMethod(receipt.capture_method),
+        receiptCardBrand: formatCardBrand(receipt.card_brand),
+        receiptDate: formatDate(receipt.payment_date),
+        receipt,
+      }
+
+      return ejs.renderFile(
+        TEMPLATE_PATH,
+        data,
+        {
+          rmWhitespace: true,
+        },
+        (err, str) => {
+          renderedTemplate = str
+        }
+      )
+    })
+
+    test('should have seller name \'Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Loja 1 2 3'))
+    })
+
+    test('should have payment date \'2018-03-02 10:12:25\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('02/03/2018 - 07:12'))
+    })
+
+    test('should have line \'na fatura do seu cartão como\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('na fatura do seu cartão como'))
+    })
+
+    test('should have soft descriptor \'pg* Loja 1 2 3\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('pg* Loja 1 2 3'))
+    })
+
+    test('should have amount \'9,87\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('9,87'))
+    })
+
+    test('should have installments \'2\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('2'))
+    })
+
+    test('should have \'Bandeira\' \'Visa Crédito\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('Visa Crédito'))
+    })
+
+    test('should have \'Últimos dígitos\' \'4242\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('1234'))
+    })
+
+    test('should have \'ID da transação\' \'12345\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('12345'))
+    })
+
+    test('should have \'AUT\' \'4DDP1X\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('4DDP1X'))
+    })
+
+    test('should have \'Código do lojista\' \'loja123\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('loja123'))
+    })
+
+    test('should have \'AC\' \'ONL-CHIP\'', () => {
+      expect(renderedTemplate).toEqual(expect.stringContaining('ONL-CHIP'))
+    })
+
+    test('should not have line \'Transação autorizada mediante uso de senha pessoal', () => {
+      expect(renderedTemplate).not.toBe(expect.stringContaining('Transação autorizada mediante uso de senha pessoal'))
+    })
+  })
+})
