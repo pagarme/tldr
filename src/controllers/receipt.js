d42c8c59a3f8234d73559819bf04fb4d8f6faf59
Allan Jorge <allan.jorge@pagar.me>
2018-07-24 13:18:31 -0300

    chore: remove now deprecated seller name lib

--- src/controllers/receipt.js
+++ src/controllers/receipt.js
@@ -3,7 +3,6 @@ const responseHelper = require('../helpers/response')
 const formatMoney = require('../lib/money')
 const formatPhone = require('../lib/phone')
 const formatDate = require('../lib/date')
-const formatSellerName = require('../lib/seller-name')
 const formatPaymentMethod = require('../lib/payment-method')
 const formatCaptureMethod = require('../lib/capture-method')
 const formatCardBrand = require('../lib/card-brand')
@@ -51,7 +50,6 @@ const render = (req, res) => {
       const receiptAmount = formatMoney(receipt.amount)
       const receiptPhone = formatPhone(receipt.phone_number)
       const receiptDate = formatDate(receipt.payment_date)
-      const receiptSellerName = formatSellerName(receipt.seller_name)
       const receiptPaymentMethod = formatPaymentMethod(receipt.payment_method)
       const receiptCaptureMethod = formatCaptureMethod(receipt.capture_method)
       const receiptCardBrand = formatCardBrand(receipt.card_brand)
@@ -63,7 +61,6 @@ const render = (req, res) => {
           receiptAmount,
           receiptPhone,
           receiptDate,
-          receiptSellerName,
           receiptPaymentMethod,
           receiptCaptureMethod,
           receiptCardBrand,
