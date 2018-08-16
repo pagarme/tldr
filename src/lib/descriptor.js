dfffedbcd8c98ebcfb0150e3e59af9ee9e287830
Matheus Macabu <macabu.matheus@gmail.com>
2018-08-16 16:29:19 -0300

    lib: remove unused descriptor

--- src/lib/descriptor.js
+++
@@ -1,9 +0,0 @@
-const pickDescriptor = ({
-  statement_descriptor: statementDescriptor,
-  soft_descriptor: softDescriptor,
-  seller_name: sellerName,
-}) =>
-  statementDescriptor ||
-  `pg *${(softDescriptor || sellerName || 'Pagar.me').substring(0, 18)}`
-
-module.exports = pickDescriptor
