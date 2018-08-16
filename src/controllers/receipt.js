a1f9c7bb015f830339797f371b2a3cadc3a7b661
Matheus Macabu <macabu.matheus@gmail.com>
2018-08-16 16:28:01 -0300

    controllers: always use statement descriptor

--- src/controllers/receipt.js
+++ src/controllers/receipt.js
@@ -81,7 +81,7 @@ const render = (req, res) => {
       const receiptPaymentMethod = formatPaymentMethod(receipt.payment_method)
       const receiptCaptureMethod = formatCaptureMethod(receipt.capture_method)
       const receiptCardBrand = formatCardBrand(receipt.card_brand)
-      const receiptDescriptor = pickDescriptor(receipt)
+      const receiptDescriptor = receipt.statement_descriptor.substring(0, 18)
       const receiptLowerCardBrand = receipt.card_brand.toLowerCase()
 
       const receiptVersion = templateVersion(receipt)
