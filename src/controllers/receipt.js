1ce91e045e1c99087e632d50a2294fc09e984cdd
Allan Ramos <allangrds@gmail.com>
2019-07-03 17:31:29 -0300

    wip

--- src/controllers/receipt.js
+++ src/controllers/receipt.js
@@ -90,11 +90,15 @@ const render = (req, res) => {
       const receiptCardBrand = formatCardBrand(receipt.card_brand)
       const receiptDescriptor = pickDescriptor(receipt)
       const receiptLowerCardBrand = receipt.card_brand.toLowerCase()
+      const receiptType = receipt.type.toLowerCase()
 
-      const receiptVersion = templateVersion(receipt)
+      const fileName = receiptType === 'stone_mais'
+        ? templateVersion(receipt)
+        : 'receipt'
+      const filePath = `pages/${receiptType}/${fileName}`
 
       return res.render(
-        `pages/${receiptVersion}`,
+        filePath,
         {
           receipt,
           receiptAmount,
